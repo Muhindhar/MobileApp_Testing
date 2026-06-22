fc616b95a83006f9d07d80ecfc916359b70ccc70
Muhindhar <muhindharsv27@gmail.com>
2026-06-22 22:50:09 +0530

    Update YouTubeTest.java

--- src/test/java/com/tests/YouTubeTest.java
+++ src/test/java/com/tests/YouTubeTest.java
@@ -1,30 +1,61 @@
 package com.tests;
 
 import java.net.URL;
+import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 
 public class YouTubeTest {
 
-	public static void main(String[] args) throws Exception {
-		UiAutomator2Options options = new UiAutomator2Options();
-		options.setPlatformName("Android");
-		options.setUdid("0841131286051090");
-		options.setAppPackage("com.google.android.youtube");
-		options.setAppActivity("com.google.android.apps.youtube.app.WatchWhileActivity");
+    AndroidDriver driver;
 
-		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
-		Thread.sleep(5000);
-		driver.findElement(By.xpath("//*[@content-desc='Search']")).click();
-		Thread.sleep(3000);
-		driver.findElement(By.id("com.google.android.youtube:id/search_edit_text")).sendKeys("selenium");
-		Thread.sleep(2000);
-		driver.pressKey(new io.appium.java_client.android.nativekey.KeyEvent(
-				io.appium.java_client.android.nativekey.AndroidKey.ENTER));
-		Thread.sleep(10000);
-		driver.quit();
-	}
+    @BeforeClass
+    public void setup() throws Exception {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("Android");
+        options.setUdid("0841131286051090");
+        options.setAppPackage("com.google.android.youtube");
+        options.setAppActivity("com.google.android.apps.youtube.app.WatchWhileActivity");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void testYouTubeSearch() throws Exception {
+        Thread.sleep(5000);
+
+        By searchIcon = By.xpath("//*[@content-desc='Search']");
+        By searchBox = By.id("com.google.android.youtube:id/search_edit_text");
+
+        driver.findElement(searchIcon).click();
+        Thread.sleep(2000);
+
+        driver.findElement(searchBox).sendKeys("selenium");
+        Thread.sleep(2000);
+
+        driver.pressKey(new KeyEvent(AndroidKey.ENTER));
+        Thread.sleep(5000);
+
+        Assert.assertTrue(driver.findElements(searchBox).size() > 0,
+                "YouTube search action did not execute properly");
+
+        System.out.println("YouTube search for 'selenium' executed successfully");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
